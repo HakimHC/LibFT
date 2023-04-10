0e09802398b5e6b1f0f87ff87bd144aa996ecd02
HakimHC <ahmedh90111@gmail.com>
2023-04-10 14:02:50 +0200

    multiple definitions of ft_strchr fixed

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,31 +6,11 @@
 /*   By: hakahmed <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 16:11:21 by hakahmed          #+#    #+#             */
-/*   Updated: 2023/04/10 13:42:44 by hakim            ###   ########.fr       */
+/*   Updated: 2023/04/10 14:02:22 by hakim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-char	*ft_strchr(const char *s, int c)
-{
-	char	*d;
-	int		i;
-
-	if (!s)
-		return (0);
-	d = (char *) s;
-	i = 0;
-	while (d[i])
-	{
-		if (d[i] == (unsigned char) c)
-			return (d + i);
-		i++;
-	}
-	if (c == '\0')
-		return (d + i);
-	return (0);
-}
 
 char	*get_l(char *buff)
 {
